34e0087d2fd571bfac18532472cb83d9df3489d6
scottlibert <scottlibert@ceb06b9e-d192-11de-97ae-379497272b75>
2010-02-19 04:31:31 +0000

    Updated the camera horizontal angle of view; updated the accessor methods to return distance in feet rather than inches.

--- Source/2010/MecanumRobot/KauaibotsTarget.cpp
+++ Source/2010/MecanumRobot/KauaibotsTarget.cpp
@@ -1,7 +1,7 @@
 #include "KauaibotsTarget.h"
 #include <math.h>
 
-const double cCameraAngleOfViewDegrees = 47.0;		// TODO:  Measure this
+const double cCameraAngleOfViewDegrees = 40.0; //47.0;		// TODO:  Measure this
 const double cCameraLensHeightInches = 24.5;		// TODO:  Measure this
 const double cTargetCenterHeightInches = 56.5;
 const double cTargetOuterDiameterInches = 33.0;
@@ -11,7 +11,7 @@ const double cCameraServoMaxRange = 1;
 const double cHorizCameraServoCenterValue = 0.5;	// TODO:  Measure this
 const double cVertCameraServoCenterValue = 0.5;		// TODO:  Measure this
 const double cHorizCameraServoRangeDegrees = 90;	// TODO:  Measure this
-const double cVertCameraServoRangeDegrees = 80;		// TODO:  Measure this
+const double cVertCameraServoRangeDegrees = 80;		// -18 degrees from vertical (tilted backwards)
 
 double DegreesToRadians( double dDegrees )
 {
@@ -81,7 +81,7 @@ double KauaibotsTarget::GetVertServoAngle()
 }
 
 // Method using yPos
-double KauaibotsTarget::GetDistanceToTargetInches()
+double KauaibotsTarget::GetDistanceToTargetFeet()
 {
 	double dDistanceInches = ( (cTargetCenterHeightInches - cCameraLensHeightInches) /
 		 sin( DegreesToRadians ( fabs ( GetRobotVerticalAngle())))) / 12;
@@ -90,7 +90,7 @@ double KauaibotsTarget::GetDistanceToTargetInches()
 }
 
 // Method using yPos
-double KauaibotsTarget::GetDistanceToGoalInches()
+double KauaibotsTarget::GetDistanceToGoalFeet()
 {
 	double dDistanceInches = ( (cTargetCenterHeightInches - cCameraLensHeightInches) /
 		 tan( DegreesToRadians ( fabs ( GetRobotVerticalAngle())))) / 12;
@@ -99,9 +99,9 @@ double KauaibotsTarget::GetDistanceToGoalInches()
 }
 
 // Method using Major Radius
-double KauaibotsTarget::GetDistanceToTargetInches2()
+double KauaibotsTarget::GetDistanceToTargetFeet2()
 {
-	double dDistance = ((1/((m_nVertPixelPitchMM*(m_majorRadius*2))/cCameraFocalLengthMM))*cTargetOuterDiameterInches)/12;
+	double dDistance = ((1/((m_nVertPixelPitchMM*((m_majorRadius * m_nHeight)*2))/cCameraFocalLengthMM))*cTargetOuterDiameterInches)/12;
 	
 	return dDistance;
 }
