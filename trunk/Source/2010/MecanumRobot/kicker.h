bba9c50bc1beb9c57099b633cf641de1984871ef
scottlibert <scottlibert@ceb06b9e-d192-11de-97ae-379497272b75>
2010-02-19 06:01:34 +0000

    adjust spin and re orient axes

---
+++ trunk/Source/2010/MecanumRobot/kicker.h
@@ -0,0 +1,14 @@
+class Kicker
+{
+protected:
+	Jaguar WinchControl;
+	Jaguar TensionControl;
+	Potentiometer Tension;
+	AnologInput LightSensor;
+	DigitalInput WinchLoaded;
+	
+public:
+	Kicker()
+
+	
+	
