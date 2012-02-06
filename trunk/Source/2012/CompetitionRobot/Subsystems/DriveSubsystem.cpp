95a51a165b97457883dedd12b62ee36374ae2406
scottlibert@gmail.com <scottlibert@gmail.com@ceb06b9e-d192-11de-97ae-379497272b75>
2012-02-06 16:28:34 +0000

    Added support for gyros, rangefinders and bridge edge finders. Modified to inherit from PIDSubsystem, so that the "rotation" PID controller is in place.

--- trunk/Source/2012/CompetitionRobot/Subsystems/DriveSubsystem.cpp
+++ trunk/Source/2012/CompetitionRobot/Subsystems/DriveSubsystem.cpp
@@ -2,12 +2,42 @@
 #include "../Robotmap.h"
 #include "../Commands/DriveWithJoystick.h"
 
+#define DRIVE_FRONT_EDGEFINDER_CHANNEL 4
+#define DRIVE_RIGHT_EDGEFINDER_CHANNEL 5
+#define DRIVE_REAR_EDGEFINDER_CHANNEL 6
+#define DRIVE_LEFT_EDGEFINDER_CHANNEL 7
+
+
 DriveSubsystem::DriveSubsystem() : 
-	Subsystem("DriveSubsystem"),
-	drive(LEFT_FRONT_CAN_ADDRESS,RIGHT_FRONT_CAN_ADDRESS,LEFT_REAR_CAN_ADDRESS,RIGHT_REAR_CAN_ADDRESS,CANJaguar::kSpeed)
+	PIDSubsystem("DriveSubsystem",0.024,0.0012,0.00),
+	drive(LEFT_FRONT_CAN_ADDRESS,RIGHT_FRONT_CAN_ADDRESS,LEFT_REAR_CAN_ADDRESS,RIGHT_REAR_CAN_ADDRESS,CANJaguar::kSpeed),
+	yaw(DRIVE_YAW_GYRO_CHANNEL),
+	pitch(DRIVE_PITCH_GRYO_CHANNEL),
+	roll(DRIVE_ROLL_GYRO_CHANNEL),
+	frontRanger(FRONT_RANGEFINDER_CHANNEL),
+	rightRanger(RIGHT_RANGEFINDER_CHANNEL),
+	rearRanger(REAR_RANGEFINDER_CHANNEL),
+	leftRanger(LEFT_RANGEFINDER_CHANNEL),
+	frontLeftEdgeFinder(DRIVE_FRONT_LEFT_EDGEFINDER_CHANNEL),
+	frontRightEdgeFinder(DRIVE_FRONT_RIGHT_EDGEFINDER_CHANNEL),
+	rearLeftEdgeFinder(DRIVE_REAR_LEFT_EDGEFINDER_CHANNEL),
+	rearRightEdgeFinder(DRIVE_REAR_RIGHT_EDGEFINDER_CHANNEL)
 {	
+	InitializeSensors();
 }
     
+void DriveSubsystem::InitializeSensors()
+{
+    // Gyroscopes
+	//         
+	// Gyro is initialized, including calibration, in constructor
+	//         
+	// Initialize sensivity of the AD22305 Gyroscope         
+	yaw.SetSensitivity(.007);  	
+	pitch.SetSensitivity(.007);  	
+	roll.SetSensitivity(.007);  	
+}
+
 void DriveSubsystem::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	SetDefaultCommand(new DriveWithJoystick());
@@ -29,3 +59,40 @@ void DriveSubsystem::setMode( CANJaguar::ControlMode newMode )
 {
 	drive.SetMode( newMode );
 }
+
+void DriveSubsystem::GetEulerAngles( double& yawAngle, double& pitchAngle, double& rollAngle)
+{
+	yawAngle = yaw.GetAngle();
+	pitchAngle = pitch.GetAngle();
+	rollAngle = roll.GetAngle();
+}
+
+void DriveSubsystem::GetRangesInches( double& frontRange, double& rightRange, double& rearRange, double& leftRange )
+{
+	frontRange =	frontRanger.GetRangeInches();
+	rightRange =	rightRanger.GetRangeInches();
+	rearRange  =	rearRanger.GetRangeInches();
+	leftRange  =	leftRanger.GetRangeInches();
+}
+
+void DriveSubsystem::GetEdges( bool &frontLeft, bool& frontRight, bool& rearRight, bool& rearLeft)
+{
+	frontLeft	= (frontLeftEdgeFinder.Get() != 0);
+	frontRight	= (frontRightEdgeFinder.Get() != 0);
+	rearLeft	= (rearLeftEdgeFinder.Get() != 0);
+	rearRight	= (rearRightEdgeFinder.Get() != 0);
+}
+
+double DriveSubsystem::ReturnPIDInput()
+{
+	return yaw.GetAngle();
+}
+
+void DriveSubsystem::UsePIDOutput(double)
+{
+	// Do nothing for now.
+	// However, when in auto-rotation mode, update
+	// the next rotation amount with this value.
+}
+
+
--- trunk/Source/2012/CompetitionRobot/Subsystems/DriveSubsystem.h
+++ trunk/Source/2012/CompetitionRobot/Subsystems/DriveSubsystem.h
@@ -3,17 +3,34 @@
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
 #include "MecanumDrive.h"
+#include "RangeFinder.h"
 
 /**
  *
  *
  * @author slibert
  */
-class DriveSubsystem: public Subsystem {
+class DriveSubsystem: public PIDSubsystem {
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
-	MecanumDrive drive;
+	MecanumDrive	drive;
+	Gyro			yaw;
+	Gyro			pitch;
+	Gyro			roll;
+	RangeFinder		frontRanger;
+	RangeFinder		rightRanger;
+	RangeFinder		rearRanger;
+	RangeFinder		leftRanger;
+	DigitalInput	frontLeftEdgeFinder;
+	DigitalInput	frontRightEdgeFinder;
+	DigitalInput	rearLeftEdgeFinder;
+	DigitalInput	rearRightEdgeFinder;
+protected:
+	void InitializeSensors();
+	double ReturnPIDInput();
+	void UsePIDOutput(double);
+
 public:
 	DriveSubsystem();
 	void InitDefaultCommand();
@@ -21,7 +38,9 @@ public:
 	void DoMecanum( float vX, float vY, float vRot );
 	CANJaguar::ControlMode getMode();
 	void setMode( CANJaguar::ControlMode );
-
+	void GetEulerAngles( double& yaw, double& pitch, double& roll);
+	void GetRangesInches( double& frontRange, double& rightRange, double& rearRange, double& leftRange );
+	void GetEdges( bool &frontLeft, bool& frontRight, bool& rearRight, bool& rearLeft);
 };
 
 #endif
