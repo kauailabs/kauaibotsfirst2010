8d208d0c02361dbdd67fbe09766a17023832aa49
MickVoigt <MickVoigt@ceb06b9e-d192-11de-97ae-379497272b75>
2012-02-19 01:41:17 +0000

    TrafficCopSubsystem first commit

---
+++ Source/2012/TrafficCopTest/Commands/TrafficCop.h
@@ -0,0 +1,21 @@
+#ifndef TRAFFIC_COP_H
+#define TRAFFIC_COP_H
+
+#include "../CommandBase.h"
+
+/**
+ *
+ *
+ * @author ExampleAuthor
+ */
+class TrafficCop: public CommandBase {
+public:
+	TrafficCop();
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+};
+
+#endif
---
+++ Source/2012/TrafficCopTest/Subsystems/TrafficCopSubsystem.cpp
@@ -0,0 +1,35 @@
+#include "TrafficCopSubsystem.h"
+#include "../Robotmap.h"
+
+TrafficCopSubsystem::TrafficCopSubsystem() : Subsystem("TrafficCopSubsystem") 
+{
+
+}
+    
+void TrafficCopSubsystem::InitDefaultCommand() 
+{
+	// Set the default command for a subsystem here.
+	//SetDefaultCommand(new MySpecialCommand());
+}
+
+void TrafficCopSubsystem::Forward()
+{
+	forward->Set(Relay::kForward);
+	
+}
+void TrafficCopSubsystem::Back()
+{
+	back->Set(Relay::kReverse);
+}
+void TrafficCopSubsystem::Stop()
+{
+	stop->Set(Relay::kOn);
+}
+void TrafficCopSubsystem::Off()
+{
+	off->Set(Relay::kOff);
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
+
