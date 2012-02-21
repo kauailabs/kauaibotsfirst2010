96040a54804cb7677283d6a34b962781b1aa68da
scottlibert@gmail.com <scottlibert@gmail.com@ceb06b9e-d192-11de-97ae-379497272b75>
2012-02-21 21:34:14 +0000

    Updated w/changes made during system integration testing on 2/20/2012 - the last day of testing at the practice field before we shipped the robot.

--- trunk/Source/2012/IntegratedTest/Commands/Gobbler.cpp
+++ trunk/Source/2012/IntegratedTest/Commands/Gobbler.cpp
@@ -21,7 +21,7 @@ void Gobbler::Execute() {
 	bool forwardshootgobbler=yjoystick->GetRawButton(3);
 	if(forwardgobbler or forwardshootgobbler)
 	{
-		gobbler->Forward();
+		gobbler->Reverse();
 	}
 	else
 	{
