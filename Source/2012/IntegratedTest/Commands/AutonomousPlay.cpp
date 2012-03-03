3b87d82ed40f71a5fc8881a20c70552c1cd123df
Tznerd13 <Tznerd13@ceb06b9e-d192-11de-97ae-379497272b75>
2012-03-03 22:09:57 +0000

    There are afew errors in the current autonomous code, but I can't yet figure out how to pull the data from the robot for the rangefinders, and how to set the rpms through another command.

--- Source/2012/IntegratedTest/Commands/AutonomousPlay.cpp
+++ Source/2012/IntegratedTest/Commands/AutonomousPlay.cpp
@@ -8,12 +8,13 @@ AutonomousPlay::AutonomousPlay() {
         //      AddSequential(new Command2());
         // these will run in order.
 		AddSequential(new DriveForward());
+		AddParallel(new BridgeTilterDown());
         // To run multiple commands at the same time,
         // use AddParallel()
         // e.g. AddParallel(new Command1());
         //      AddSequential(new Command2());
         // Command1 and Command2 will run in parallel.
-		AddParallel(new BridgeTilterDown());
+		
         // A command group will require all of the subsystems that each member
         // would require.
         // e.g. if Command1 requires chassis, and Command2 requires arm,
