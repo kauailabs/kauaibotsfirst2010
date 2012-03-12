ffaba00715ef0041144b618e37b14b65fdd57048
scottlibert@gmail.com <scottlibert@gmail.com@ceb06b9e-d192-11de-97ae-379497272b75>
2012-03-12 04:09:10 +0000

    Cleaned up the code a bit, added comments.

--- trunk/Source/2012/IntegratedTest/CommandBasedRobot.cpp
+++ trunk/Source/2012/IntegratedTest/CommandBasedRobot.cpp
@@ -19,8 +19,11 @@ private:
 	
 	virtual void RobotInit() {
 		CommandBase::init();
+		
 		//NetworkTable::Initialize();
+		
 		SmartDashboard*sd=SmartDashboard::GetInstance();
+		
 		//sd->PutData(CommandBase::chute);
 		//sd->PutData(CommandBase::gobbler);
 		//sd->PutData(CommandBase::drive);
@@ -28,16 +31,30 @@ private:
 		//sd->PutData(CommandBase::tilter);
 		//sd->PutData(CommandBase::camera);
 		//sd->PutData(CommandBase::trafficcop);
-		autoChooser.AddDefault("DunkRight",new AutonomousPlayRight());
-		autoChooser.AddObject("DunkLeft",new AutonomousPlayLeft());
-		autoChooser.AddObject("DunkStraight",new AutonomousPlayStraight());
+		
+		autoChooser.AddDefault(	"DunkRight",	new AutonomousPlayRight());
+		autoChooser.AddObject(	"DunkLeft",		new AutonomousPlayLeft());
+		autoChooser.AddObject(	"DunkStraight",	new AutonomousPlayStraight());
+		
 		sd->PutData("Autonomous Mode Chooser", &autoChooser);
+		
 		autonomousCommand = 0;
 	}
 	
 	virtual void AutonomousInit() {
+		
+		// Set the iteration time period to 20 ms; this is more 
+		// accurate than the default period (0.0), which executes
+		// each iteration whenever a new packet is received from
+		// the drive station.
+		
 		SetPeriod(0.02);
 		autonomousCommand = (Command*)autoChooser.GetSelected();
+		
+		// Start() will schedule the command for execution.
+		// However, the command will not actually begin 
+		// execution until the Scheduler is run (see AutonomousPeriodic()).
+		
 		autonomousCommand->Start();
 	}
 	
@@ -46,15 +63,27 @@ private:
 	}
 	
 	virtual void TeleopInit() {
+
+		// Set the iteration time period to "0.0" ms.  This special
+		// value means that each teleop iteration (see TeleopPeriodic())
+		// will execute whenever a new packet is received from
+		// the drive station.  While not as accurate timing-wise
+		// as a non-zero period, this assures the fastest possible
+		// response to commands (e.g., joystick) from the driver station.
+		
+		SetPeriod(0.0);
+		
 		// This makes sure that the autonomous stops running when
 		// teleop starts running. If you want the autonomous to 
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
-		SetPeriod(0.0);
+		
 		if ( autonomousCommand )
 		{
 			autonomousCommand->Cancel();
+			autonomousCommand = 0;
 		}
+		
 	}
 	
 	virtual void TeleopPeriodic() {
