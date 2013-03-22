bfd9799724557b3298ca0a44b500dadc58167a3c
scottlibert@gmail.com <scottlibert@gmail.com@ceb06b9e-d192-11de-97ae-379497272b75>
2013-03-22 03:46:00 +0000

    Integrated joystick response tuning into code.

--- trunk/Source/2013/ThunderChicken2013/Commands/SwerveDrive.cpp
+++ trunk/Source/2013/ThunderChicken2013/Commands/SwerveDrive.cpp
@@ -2,6 +2,8 @@
 #include "WPILib.h"
 #include "../OI.h"
 #include "Joystick.h"
+#include <math.h>
+#include "JoystickConstants.h"
 SwerveDrive::SwerveDrive() {
 	// Use requires() here to declare subsystem dependencies
 	// eg. requires(chassis);
@@ -18,55 +20,29 @@ void SwerveDrive::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void SwerveDrive::Execute() {  
 	Joystick* pstick = Robot::oi->getdriver_joystick();
-	double twist = pstick->GetTwist();
+	double rotate = pstick->GetTwist();
 	double y = pstick->GetY();
 	double x = pstick->GetX();
 	
-	float deadband = 0.05;
+	y = -y;		// First, invert the Y axis of the joystick (by default, joystick "forward" is negative)
 	
-	if ( twist > 0 )
-	{
-		if ( twist < deadband )
-		{
-			twist = 0.0;
-		}
-	}
-	else if ( twist < 0 )
-	{
-		if ( twist > -deadband )
-		{
-			twist = 0.0;
-		}
-	}
-	if ( x > 0 )
-	{
-		if ( x < deadband )
-		{
-			x = 0.0;
-		}
-	}
-	else if ( x < 0 )
-	{
-		if ( x > -deadband )
-		{
-			x = 0.0;
-		}
-	}	
-	if ( y > 0 )
-	{
-		if ( y < deadband )
-		{
-			y = 0.0;
-		}
-	}
-	else if ( y < 0 )
-	{
-		if ( y > -deadband )
-		{
-			y = 0.0;
-		}
-	}	
-	Robot::swerveDriveSystem->DoSwerve(x,-y,twist);
+	// Adjust response of all joystick components
+	//
+	// To visualize the response, use the grapher at http://www.mathsisfun.com/data/function-grapher.php
+	//
+	// The equation to use is Multiplier*(Adjustment*JoyIn^Power + JoyIn(1-Adjustment))
+	//
+	// where the X axis input is the joystick value (range is from -1 to 1)
+	//
+	// Here's an example URL:  
+	//
+	// http://www.mathsisfun.com/graph/function-grapher.php?func1=1*(.4*x^3+x(1-.4))&xmin=-1&xmax=1&ymin=-1&ymax=1
+	
+	x = AdjustJoystickResponse( x, JoystickStrafe_Adjust, JoystickStrafe_Power, JoystickStrafe_Multiplier, JoystickStrafe_Deadband );
+	y = AdjustJoystickResponse( y, JoystickForward_Adjust, JoystickForward_Power, JoystickForward_Multiplier, JoystickForward_Deadband );
+	rotate = AdjustJoystickResponse( rotate, JoystickRotate_Adjust, JoystickRotate_Power, JoystickRotate_Multiplier, JoystickRotate_Deadband );
+	
+	Robot::swerveDriveSystem->DoSwerve(x,y,rotate);
 }
 
 // Make this return true when this Command no longer needs to run execute()
@@ -84,3 +60,13 @@ void SwerveDrive::End() {
 void SwerveDrive::Interrupted() {
 
 }
+
+double SwerveDrive::AdjustJoystickResponse( double dJoystickIn, double dAdjustment, double dExponent, double dMultiplier , double dDead) 
+{         
+	double dJoystickOut = 0.0;         
+	if ((dJoystickIn > dDead) || (dJoystickIn < (-1*dDead)))         
+	{                 
+		dJoystickOut = ((dAdjustment * pow(dJoystickIn, dExponent)) + ((1 - dAdjustment) * dJoystickIn)) * dMultiplier;          
+	}         
+	return dJoystickOut; 
+} 
