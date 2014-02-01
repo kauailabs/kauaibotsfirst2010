c7cbdf0b8615119241f22e12aa0a1ff796d674c3
scottlibert@gmail.com <scottlibert@gmail.com@ceb06b9e-d192-11de-97ae-379497272b75>
2014-02-01 04:32:10 +0000

    Refactored the Arms, Tensioner and Ankle Subsystems to inherit from PIDSubsystem, allowing PID tuning in the test mode of the dashboard.
    Also, added various preferences to the RobotPreferences class.

--- trunk/Source/2014/CompetitionRobot/Robot/src/org/usfirst/frc2465/Robot/RobotPreferences.java
+++ trunk/Source/2014/CompetitionRobot/Robot/src/org/usfirst/frc2465/Robot/RobotPreferences.java
@@ -16,29 +16,79 @@ public class RobotPreferences {
     
     /* Arm Preferences */
     
-    static public double getMaxArmAngle() { 
-        return Preferences.getInstance().getDouble("MaxArmAngle",110.0);
+    static public double getArmsMaxAngle() { 
+        return Preferences.getInstance().getDouble("ArmsMaxAngle",110.0);
     }
-    static public double getMinArmAngle() {
-        return Preferences.getInstance().getDouble("MinArmAngle",0);
+    static public double getArmsMinAngle() {
+        return Preferences.getInstance().getDouble("ArmsMinAngle",0);
     }
-    static public double getMaxArmSensorVolts() {
-        return Preferences.getInstance().getDouble("MaxArmSensorVolts",1.6);
+    static public double getArmsMaxSensorVolts() {
+        return Preferences.getInstance().getDouble("ArmsMaxSensorVolts",1.6);
     }
-    static public double getMinArmSensorVolts() {
-        return Preferences.getInstance().getDouble("MinArmSensorVolts",3.1);        
+    static public double getArmsMinSensorVolts() {
+        return Preferences.getInstance().getDouble("ArmsMinSensorVolts",3.1);        
     }
-    static public double getLowArmAngle() {
-        return Preferences.getInstance().getDouble("ArmAngleLow",0.0);        
+    static public double getArmsAngleLow() {
+        return Preferences.getInstance().getDouble("ArmsAngleLow",0.0);        
     }
-    static public double getMidArmAngle() {
-        return Preferences.getInstance().getDouble("ArmAngleLow",45.0);        
+    static public double getArmsAngleMid() {
+        return Preferences.getInstance().getDouble("ArmsAngleMid",45.0);        
     }
-    static public double getHighArmAngle() {
-        return Preferences.getInstance().getDouble("ArmAngleLow",90.0);        
+    static public double getArmsAngleHigh() {
+        return Preferences.getInstance().getDouble("ArmsAngleHigh",90.0);        
     }
-    static public double getUpArmAngle() {
-        return Preferences.getInstance().getDouble("ArmAngleLow",110.0);        
+    static public double getArmsAngleUp() {
+        return Preferences.getInstance().getDouble("ArmsAngleUp",110.0);        
+    }
+    static public double getArmsP() {
+        return Preferences.getInstance().getDouble("ArmsP", 0.1);
+    }
+    static public double getArmsI() {
+        return Preferences.getInstance().getDouble("ArmsI", 0.01);
+    }
+    static public double getArmsD() {
+        return Preferences.getInstance().getDouble("ArmsD", 0.0);
+    }
+
+    /* Ankle Preferences */
+    
+    static public double getAnkleP() {
+        return Preferences.getInstance().getDouble("AnkleP", 0.1);
+    }
+    static public double getAnkleI() {
+        return Preferences.getInstance().getDouble("AnkleI", 0.01);
+    }
+    static public double getAnkleD() {
+        return Preferences.getInstance().getDouble("AnkleD", 0.0);
+    }
+    static public double getAnkleHeightInches() {
+        return Preferences.getInstance().getDouble("AnkleHeightInches",0.0);
+    }
+    static public double getAnkleBaselineInches() {
+        return Preferences.getInstance().getDouble("AnkleBaselineInches",6.0);
+    }
+    static public double getAnkleDefaultAngle() {
+        return Preferences.getInstance().getDouble("AnkleDefaultAngle",45.0);        
+    }
+    /* Tensioner Preferences */
+    
+    static public double getTensionerP() {
+        return Preferences.getInstance().getDouble("TensionerP", 0.1);
+    }
+    static public double getTensionerI() {
+        return Preferences.getInstance().getDouble("TensionerI", 0.01);
+    }
+    static public double getTensionerD() {
+        return Preferences.getInstance().getDouble("TensionerD", 0.0);
+    }    
+    static public double getTensionerDefaultDistanceInches() {
+        return Preferences.getInstance().getDouble("TensionerDefaultDistanceInches",6.0);
+    }
+    static public double getTensionerMinDistanceInches() {
+        return Preferences.getInstance().getDouble("TensionerMinDistanceInches",0.0);
+    }
+    static public double getTensionerMaxDistanceInches() {
+        return Preferences.getInstance().getDouble("TensionerMaxDistanceInches",12.0);
     }
     
 }
